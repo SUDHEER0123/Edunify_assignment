7621d02449e79a0005221a8e5f8ed5107f8d4cf2
SUDHEER0123 <sudheerrec.ee@gmail.com>
2024-01-08 22:40:33 +0530

    added .env file

--- configuration/db.js
+++ configuration/db.js
@@ -1,11 +1,12 @@
 import mysql from "mysql2/promise";
+const { DB_USERNAME, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
 
 const dbconnection = await mysql.createConnection({
-  host: "sql12.freesqldatabase.com",
-  user: "sql12675500",
-  password: "DJpFPgtdQP",
-  port: 3306,
-  database: "sql12675500",
+  host: DB_HOST,
+  user: DB_USERNAME,
+  password: DB_PASSWORD,
+  port: DB_PORT,
+  database: DB_NAME,
 });
 
 export default dbconnection;
